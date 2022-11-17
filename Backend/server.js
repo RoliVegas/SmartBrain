9e55172098c6a510543fd5021f3924afab38bcfb
RoliVegas <roland.krancz@gmail.com>
2022-11-17 13:05:50 +0100

    .

--- Backend/server.js
+++ Backend/server.js
@@ -12,14 +12,12 @@ const clarifaiApp = new Clarifai.App({
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
-
+app.use( (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, X-Auth-Token, Accept");
     next();
 });
-
 
 app.get('/', (request, response) => {
     response.send('Running.');
